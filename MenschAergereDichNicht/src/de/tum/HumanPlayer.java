49bf4a707333566e7ea6efa83725962829efd786
Stefan.Matl@gmail.com <Stefan.Matl@gmail.com@3a0991a5-c2ad-d7c1-b131-875bdd8fb457>
2011-12-26 16:46:31 +0000

    human player changed

--- MenschAergereDichNicht/src/de/tum/HumanPlayer.java
+++ MenschAergereDichNicht/src/de/tum/HumanPlayer.java
@@ -1,5 +1,28 @@
 package de.tum;
 
-public class HumanPlayer {
+/**
+ * this player is a human player. All moves are chosen by a human
+ */
+public class HumanPlayer extends Player {
+	/**
+	 * creating a player
+	 * 
+	 * @param team
+	 *            the team of this player
+	 */
+	public HumanPlayer(Team team) {
+		super(team);
+	}
 
+	/** {@inheritDoc} */
+	protected void throwDice() {
+		Dice.throwIt();
+		// ############################### needs some change
+	}
+
+	/** {@inheritDoc} */
+	protected void choosePegForMove(Peg[] movables) {
+		pegChosen(movables[0]);
+		// ############################### needs some change
+	}
 }
