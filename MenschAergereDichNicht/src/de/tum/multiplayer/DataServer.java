6dfd02625d913f2dc8b200154e4e8e84279a5ff2
boztop <boztop@gmail.com>
2012-01-22 18:58:15 +0000

    One bug is fixed about progress dialog, and gave the structure of sending gam data between devices.

--- MenschAergereDichNicht/src/de/tum/multiplayer/DataServer.java
+++ MenschAergereDichNicht/src/de/tum/multiplayer/DataServer.java
@@ -1,5 +1,15 @@
 package de.tum.multiplayer;
 
+import de.tum.models.Board;
+import de.tum.player.Player;
+
 public class DataServer {
+	
+	public Board board;
+	public Player[] player;
+	
+	public DataServer(){
+		
+	}
 
 }
