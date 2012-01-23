5ac78fe97f0cffb834f918248587511b1acbec55
boztop@gmail.com <boztop@gmail.com@3a0991a5-c2ad-d7c1-b131-875bdd8fb457>
2012-01-23 20:26:34 +0000

    after our conversation

--- MenschAergereDichNicht/src/de/tum/multiplayer/DataClient.java
+++ MenschAergereDichNicht/src/de/tum/multiplayer/DataClient.java
@@ -3,5 +3,24 @@ package de.tum.multiplayer;
 import java.io.Serializable;
 
 public class DataClient implements Serializable{
+	
+	private String same;
+
+	public DataClient(String same) {
+		this.same = same;
+	}
+	
+	public void setValue(String same) {
+		this.same = same;
+	}
+	
+	public String getValue() {
+		return same;
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
 }
