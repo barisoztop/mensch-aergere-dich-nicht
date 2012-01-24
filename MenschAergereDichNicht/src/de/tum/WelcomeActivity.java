325f35cef67cb1d9e0bc5589ec92fe5e3f3babce
boztop <boztop@gmail.com>
2012-01-24 00:01:16 +0000

    ABOUT and SETTINGS screens are added. We will modify according to our needs.

--- MenschAergereDichNicht/src/de/tum/WelcomeActivity.java
+++ MenschAergereDichNicht/src/de/tum/WelcomeActivity.java
@@ -1,5 +1,6 @@
 package de.tum;
 
+import de.tum.R;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,108 +11,117 @@ import android.view.Window;
 import android.widget.Button;
 import de.tum.multiplayer.MultiplayerActivity;
 
-public class WelcomeActivity extends Activity{
-	
-    // Debugging
-    private static final String TAG = "WelcomeActivity";
-    private static final boolean D = true;
-	
+public class WelcomeActivity extends Activity {
+
+	// Debugging
+	private static final String TAG = "WelcomeActivity";
+	private static final boolean D = true;
+
 	private Button newGameButton;
 	private Button multiplayerGameButton;
 	private Button settingsButton;
 	private Button aboutButton;
 	private Button exitButton;
-	
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.welcome);
-        
-        // Initialize the buttons
-        newGameButton = (Button) findViewById(R.id.newgame);
-        multiplayerGameButton = (Button) findViewById(R.id.multiplayer);
-        settingsButton = (Button) findViewById(R.id.settings);
-        aboutButton = (Button) findViewById(R.id.about);
-        exitButton = (Button) findViewById(R.id.exit);
-        
-        newGameButton.setOnClickListener(new OnClickListener() {
-			
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		// getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+		// WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		setContentView(R.layout.welcome);
+
+		// Initialize the buttons
+		newGameButton = (Button) findViewById(R.id.newgame);
+		multiplayerGameButton = (Button) findViewById(R.id.multiplayer);
+		settingsButton = (Button) findViewById(R.id.settings);
+		aboutButton = (Button) findViewById(R.id.about);
+		exitButton = (Button) findViewById(R.id.exit);
+
+		newGameButton.setOnClickListener(new OnClickListener() {
+
 			@Override
 			public void onClick(View v) {
-				Intent newGame = new Intent(getApplicationContext(), MenschAergereDichNichtActivity.class);
+				Intent newGame = new Intent(getApplicationContext(),
+						MenschAergereDichNichtActivity.class);
 				startActivity(newGame);
-				
+
 			}
 		});
-        
-        multiplayerGameButton.setOnClickListener(new OnClickListener() {
-			
+
+		multiplayerGameButton.setOnClickListener(new OnClickListener() {
+
 			@Override
 			public void onClick(View v) {
-				Intent multiplayerGame = new Intent(getApplicationContext(), MultiplayerActivity.class);
+				Intent multiplayerGame = new Intent(getApplicationContext(),
+						MultiplayerActivity.class);
 				startActivity(multiplayerGame);
-				
 			}
 		});
-        
-        settingsButton.setOnClickListener(new OnClickListener() {
-			
+
+		settingsButton.setOnClickListener(new OnClickListener() {
+
 			@Override
 			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				
+				Intent gameSettings = new Intent(getApplicationContext(),
+						SettingsActivity.class);
+				startActivity(gameSettings);
 			}
 		});
-        
-        aboutButton.setOnClickListener(new OnClickListener() {
-			
+
+		aboutButton.setOnClickListener(new OnClickListener() {
+
 			@Override
 			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				
+				Intent aboutApp = new Intent(getApplicationContext(),
+						AboutActivity.class);
+				startActivity(aboutApp);
 			}
 		});
-        
-        exitButton.setOnClickListener(new OnClickListener() {
-			
+
+		exitButton.setOnClickListener(new OnClickListener() {
+
 			@Override
 			public void onClick(View v) {
 				finish();
-				
+
 			}
 		});
-    }
-    
-    @Override
-    public void onStart() {
-      super.onStart();
-      if(D) Log.e(TAG, "- ON START -");      
-    }
-    
-    @Override
-    public void onResume() {
-      super.onResume();
-      if(D) Log.e(TAG, "- ON RESUME -"); 
-    }
-    
-    @Override
-    public void onPause() {
-      super.onPause();
-      if(D) Log.e(TAG, "- ON PAUSE -");      
-    }
-    
-    @Override
-    public void onStop() {
-        super.onStop();
-        if(D) Log.e(TAG, "-- ON STOP --");
-    }
-    
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        if(D) Log.e(TAG, "--- ON DESTROY ---");
-    }      
+	}
+
+	@Override
+	public void onStart() {
+		super.onStart();
+		if (D)
+			Log.e(TAG, "- ON START -");
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+		if (D)
+			Log.e(TAG, "- ON RESUME -");
+	}
+
+	@Override
+	public void onPause() {
+		super.onPause();
+		if (D)
+			Log.e(TAG, "- ON PAUSE -");
+	}
+
+	@Override
+	public void onStop() {
+		super.onStop();
+		if (D)
+			Log.e(TAG, "-- ON STOP --");
+	}
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		if (D)
+			Log.e(TAG, "--- ON DESTROY ---");
+	}
 }
