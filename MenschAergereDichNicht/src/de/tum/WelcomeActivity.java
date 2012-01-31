76c56c68a298571d556399e367bef9f408066c82
boztop@gmail.com <boztop@gmail.com@3a0991a5-c2ad-d7c1-b131-875bdd8fb457>
2012-01-31 16:02:44 +0000

    SinglePlayer Mode: Title bar is removed, because we don't need it. Toast messages in Multiplayer mode is also applied in here.

--- MenschAergereDichNicht/src/de/tum/WelcomeActivity.java
+++ MenschAergereDichNicht/src/de/tum/WelcomeActivity.java
@@ -29,8 +29,6 @@ public class WelcomeActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
-		// getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-		// WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		setContentView(R.layout.welcome);
 
 		// Initialize the buttons
