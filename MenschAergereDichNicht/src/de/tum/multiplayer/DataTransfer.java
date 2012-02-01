2b72c5572aac63f65b25401b4759c8905026d795
boztop <boztop@gmail.com>
2012-02-01 14:26:00 +0000

    I've added progress bar to the client side as well. This is also an example of how to send individual messages to clients.

--- MenschAergereDichNicht/src/de/tum/multiplayer/DataTransfer.java
+++ MenschAergereDichNicht/src/de/tum/multiplayer/DataTransfer.java
@@ -6,8 +6,9 @@ public class DataTransfer implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	/** notification for players */
-	public static final transient int IS_NOTIFICATION = 0; 
-	public static final transient int SETUP_GAME = 1; 
+	public static final transient int IS_NOTIFICATION = 0;
+	public static final transient int SETUP_GAME = 1;
+	public static final transient int CONNECTION_STATUS = 2; 
 	
 	public final int reason;
 	public final int[] tokens;
